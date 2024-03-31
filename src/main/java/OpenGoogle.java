154bc7867f47afb8c9cf3a9f835ca9450cd313dd
Shubham-sken <shubhamlohra@salesken.ai>
2024-03-31 13:33:01 +0530

    First Commit

---
+++ src/main/java/OpenGoogle.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenGoogle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\SALESKEN\\selenium\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+//		Open Naukari.com
+		driver.get("https://www.google.com/");
+		System.out.println("----> Opened Google.com");
+	}
+
+}

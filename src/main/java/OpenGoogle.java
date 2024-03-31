7475b41752be80737c4114aedaf4407e1e46e9f9
Shubham-sken <shubhamlohra@salesken.ai>
2024-03-31 14:13:21 +0530

    Changed main method

--- src/main/java/OpenGoogle.java
+++ src/main/java/OpenGoogle.java
@@ -1,16 +1,14 @@
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OpenGoogle {
-	public static void Main(String args[]) {
-		// TODO Auto-generated method stub
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\SALESKEN\\selenium\\chromedriver.exe");
+	ChromeDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
 
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\SALESKEN\\selenium\\chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-
-//		Open Naukari.com
-		driver.get("https://www.google.com/");
-		System.out.println("----> Opened Google.com");
-	}
+//	Open Naukari.com
+	driver.get("https://www.google.com/");
+	System.out.println("----> Opened Google.com");
+}
 
 }
